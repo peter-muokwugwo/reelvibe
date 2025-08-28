e9ec5c2a246d47ddcc9d671358de2aeec60d71db
peter-muokwugwo <peter.chiejina@outlook.com>
2025-08-28 12:57:22 -0400

    Update App to include Search and MovieCard components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react'
+import Search from './components/Search';
+import MovieCard from './components/MovieCard';
+import Header from './components/Header';
 
 
-interface Movie {
+export interface Movie {
   id: string | number;
   title: string;
   posterPath: string;
+  searchTerm: string;
+  voteAverage: number;
+  originalLanguage: string;
   overview?: string;
   releaseDate?: string;
 }
 
 interface State {
-  searchTerm: string;
   movies: Movie[];
   trendingMovies: Movie[];
   error: string | null;
@@ -22,9 +27,53 @@ const App = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [trendingMovies, setTrendingMovies] = useState<Movie[]>([]);
   const [error, setError] = useState<string | null>(null);
-  
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   return (
-    <div>App</div>
+    <main>
+      <Header />
+      <section id='search'>
+        <Search 
+        search={searchTerm}
+        setSearch={setSearchTerm}
+        />
+      </section>
+      {trendingMovies.length > 0 && (
+        <section id='trending'>
+          <h2>Trending Movies</h2>
+
+          <ul>
+            {trendingMovies.map((movie, i) => (
+              <li key={movie.id}>
+                <p>{i + 1}</p>
+
+                <img 
+                  src={movie.posterPath}
+                  alt={movie.searchTerm}
+                  />
+              </li>
+            ))}
+          </ul>
+        </section>
+      )}
+      <section id='all-movie'>
+        <h2>All Reels</h2>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>{error}</p>
+        ) : (
+          <ul>
+            {movies.map(movie => (
+              <MovieCard
+                key={movie.id}
+                movie={movie}
+                />
+            ))}
+          </ul>
+        )}
+      </section>
+    </main>
   )
 }
 
