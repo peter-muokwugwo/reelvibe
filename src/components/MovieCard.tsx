cea88dea95b445732d4ec9f50b9f32321b8a741d
peter-muokwugwo <peter.chiejina@outlook.com>
2025-08-28 12:23:09 -0400

    Implement MovieCard component

---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+
+interface MovieCardProps {
+    title: string;
+    posterPath: string;
+    releaseDate: string;
+    voteAverage: number;
+    originalLanguage: string;
+}
+
+const MovieCard: React.FC<MovieCardProps> = ({ title, posterPath, releaseDate, voteAverage, originalLanguage}) => {
+  return (
+    <div className='movie-card'>
+        <img
+            src={
+                posterPath
+                ? `https://image.tmdb.org/t/p/w500/${posterPath}`
+                : './no-poster.png'
+            }
+            alt={title}
+            />
+
+            <h3>{title}</h3>
+
+            <div className='rating'>
+                <img
+                    src='./rating.svg'
+                    alt='rating star'
+                    />
+                    <p>{voteAverage ? voteAverage.toFixed(1) : 'N/A'}</p>
+            </div>
+
+            <div>
+                <span>.</span>
+                <p>{releaseDate ? releaseDate.split('-')[0] : 'N/A'}</p>
+            </div>
+    </div>
+  )
+}
+
+export default MovieCard
