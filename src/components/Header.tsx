0cb0c81132b1033df342a46db17fb280dbcb0ba9
peter-muokwugwo <peter.chiejina@outlook.com>
2025-08-28 11:45:40 -0400

    Implement Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <img src="./logo.svg" alt="reelvibe logo" />
+        <img src="./hero-img.png" alt="reelvibe hero" />
+        <h1>Find the Movies You'll Enjoy Without the Hassle.</h1>
+      </header>
+  )
+}
+
+export default Header
