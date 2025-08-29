c61b7f37de1e6b26b3638310d465e761df84e0b4
peter-muokwugwo <peter.chiejina@outlook.com>
2025-08-29 04:32:28 -0400

    Add styles to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header>
-        <img src="./reel.svg" alt="reelvibe logo" className='w-10 h-10' />
+    <div className='wrapper'>
+      <header>
+          <img src="./reel.svg" alt="reelvibe logo" className='w-24 h-24' />
         <img src="./hero-img.png" alt="reelvibe hero" />
-        <h1>Find the Movies You'll Enjoy Without the Hassle.</h1>
+        <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle.</h1>
       </header>
+    </div>
   )
 }
 
